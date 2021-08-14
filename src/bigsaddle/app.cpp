37a4d8a59403123cfbd2aebfdd9305744f0f81a7
Kurtis Fields <kurtisfields@gmail.com>
2021-08-13 23:42:10 -0400

    enable DRI3 in your Xorg config?

--- src/bigsaddle/app.cpp
+++ src/bigsaddle/app.cpp
@@ -14,6 +14,8 @@
 
 namespace bigsaddle {
 
+static void setup_bgfx_platform_data(bgfx::PlatformData &pd, const SDL_SysWMinfo &wmi);
+
 App::~App() {
     paint_thread_.detach();
 }
@@ -23,8 +25,12 @@ void App::DoCreate(CreateParams params) {
     auto result = bgfx::renderFrame(); // single threaded mode
 
     bgfx::PlatformData pd{};
+    SDL_SysWMinfo wmInfo;
+    SDL_VERSION(&wmInfo.version);
+    SDL_GetWindowWMInfo(window_, &wmInfo);
 
-    pd.nwh = GetHandle();
+    setup_bgfx_platform_data(pd, wmInfo);
+    //pd.nwh = GetHandle();
 
     bgfx::Init bgfx_init;
     bgfx_init.type = bgfx::RendererType::Count; // auto choose renderer
@@ -99,6 +105,73 @@ void App::Run() {
             break;
         }
     }
+}
+
+static void setup_bgfx_platform_data(bgfx::PlatformData &pd, const SDL_SysWMinfo &wmi) {
+    switch (wmi.subsystem) {
+        case SDL_SYSWM_UNKNOWN: abort();
+
+#if defined(SDL_VIDEO_DRIVER_X11)
+        case SDL_SYSWM_X11:
+            pd.ndt = wmi.info.x11.display;
+            pd.nwh = (void *)(uintptr_t)wmi.info.x11.window;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_WAYLAND)
+        case SDL_SYSWM_WAYLAND:
+            pd.ndt = wmi.info.wl.display;
+            pd.nwh = wmi.info.wl.surface;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_MIR)
+        case SDL_SYSWM_MIR:
+            pd.ndt = wmi.info.mir.connection;
+            pd.nwh = wmi.info.mir.surface;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_COCOA)
+        case SDL_SYSWM_COCOA:
+            pd.ndt = NULL;
+            pd.nwh = wmi.info.cocoa.window;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_UIKIT)
+        case SDL_SYSWM_UIKIT:
+            pd.ndt = NULL;
+            pd.nwh = wmi.info.uikit.window;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_WINDOWS)
+        case SDL_SYSWM_WINDOWS:
+            pd.ndt = NULL;
+            pd.nwh = wmi.info.win.window;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_WINRT)
+        case SDL_SYSWM_WINRT:
+            pd.ndt = NULL;
+            pd.nwh = wmi.info.winrt.window;
+            break;
+#endif
+
+#if defined(SDL_VIDEO_DRIVER_VIVANTE)
+        case SDL_SYSWM_VIVANTE:
+            pd.ndt = wmi.info.vivante.display;
+            pd.nwh = wmi.info.vivante.window;
+            break;
+#endif
+
+        //default: spdlog::critical("Unknown Window system."); std::abort();
+    }
+    pd.context = NULL;
+    pd.backBuffer = NULL;
+    pd.backBufferDS = NULL;
 }
 
 } //namespace bigsaddle
