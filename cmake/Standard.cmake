adddb66fc951b46f341f529f6b265df365b911db
Kurtis Fields <kurtisfields@gmail.com>
2021-08-18 23:04:44 -0400

    wslg+x11+vulkan working!

--- cmake/Standard.cmake
+++ cmake/Standard.cmake
@@ -6,7 +6,13 @@ include(${CMAKE_CURRENT_LIST_DIR}/Config.cmake)
 
 function(USES_STD THIS)
     if(${SHELL_PLATFORM} STREQUAL "Linux")
-        target_compile_definitions(${THIS} PRIVATE SDL_VIDEO_DRIVER_X11)
+        target_compile_definitions(${THIS} PRIVATE
+        SDL_VIDEO_DRIVER_X11=1
+        SDL_VIDEO_VULKAN=1
+        BGFX_CONFIG_RENDERER_VULKAN=1
+        BGFX_CONFIG_RENDERER_OPENGL=0
+        BGFX_CONFIG_RENDERER_OPENGLES=0
+        )
         #target_compile_definitions(${THIS} PRIVATE SDL_VIDEO_DRIVER_WAYLAND)
     endif()
     target_compile_features(${THIS} PUBLIC cxx_std_17)
