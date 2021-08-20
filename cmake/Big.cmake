8727750cff4527b991fd39ad3be8c44d015867be
Kurtis Fields <kurtisfields@gmail.com>
2021-08-20 10:24:42 -0400

    gl build

--- cmake/Big.cmake
+++ cmake/Big.cmake
@@ -6,10 +6,6 @@ include(${CMAKE_CURRENT_LIST_DIR}/Gui.cmake)
 function(USES_BIG THIS)
     USES_GUI(${THIS})
     USES_SHELL(${THIS})
-    #target_compile_definitions(${THIS} PRIVATE IMGUI_USER_CONFIG=<imgui/imconfig.h>)
-    #target_include_directories(${THIS} PRIVATE
-    #  ${IMGUI_ROOT}
-    #)
     target_link_libraries(${THIS} BigSaddle)
 
 endfunction()
--- cmake/Standard.cmake
+++ cmake/Standard.cmake
@@ -5,15 +5,15 @@ include_guard()
 include(${CMAKE_CURRENT_LIST_DIR}/Config.cmake)
 
 function(USES_STD THIS)
-    if(${SHELL_PLATFORM} STREQUAL "Linux")
+    if(${BIG_PLATFORM} STREQUAL "Linux")
         target_compile_definitions(${THIS} PRIVATE
-        SDL_VIDEO_DRIVER_X11=1
-        SDL_VIDEO_VULKAN=1
-        BGFX_CONFIG_RENDERER_VULKAN=1
-        BGFX_CONFIG_RENDERER_OPENGL=0
-        BGFX_CONFIG_RENDERER_OPENGLES=0
+        #SDL_VIDEO_DRIVER_X11=1
+        #SDL_VIDEO_VULKAN=1
+        #BGFX_CONFIG_RENDERER_VULKAN=1
+        #BGFX_CONFIG_RENDERER_OPENGL=0
+        #BGFX_CONFIG_RENDERER_OPENGLES=0
+        ${BIG_COMPILE_DEFS}
         )
-        #target_compile_definitions(${THIS} PRIVATE SDL_VIDEO_DRIVER_WAYLAND)
     endif()
     target_compile_features(${THIS} PUBLIC cxx_std_17)
     #target_compile_features(${THIS} PUBLIC cxx_std_20)
